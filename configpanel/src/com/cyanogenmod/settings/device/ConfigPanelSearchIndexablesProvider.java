LineageOS/android_device_xiaomi_msm8996-common

msm8996-common: ConfigPanel: Define non-indexable keys
 * This ensures that keys related with unsupported features
   are hidden from search results.

Change-Id: I2220ff21aee5d8015bea4073073492f4560e3703

configpanel/src/org/lineageos/settings/device/ConfigPanelSearchIndexablesProvider.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- *           (C) 2017 The LineageOS Project
+ *           (C) 2017-2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ import android.database.MatrixCursor;
 import android.provider.SearchIndexableResource;
 import android.provider.SearchIndexablesProvider;
 
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_NON_INDEXABLE_KEYS_KEY_VALUE;
 import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_CLASS_NAME;
 import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_ICON_RESID;
 import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_ACTION;
@@ -32,6 +33,11 @@ import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RES
 import static android.provider.SearchIndexablesContract.INDEXABLES_RAW_COLUMNS;
 import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS;
 import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
+
+import org.lineageos.internal.util.FileUtils;
+import org.lineageos.internal.util.PackageManagerUtils;
+
+import java.util.List;
 
 public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvider {
     private static final String TAG = "ConfigPanelSearchIndexablesProvider";
@@ -59,7 +65,7 @@ public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvide
     }
 
     private static Object[] generateResourceRef(SearchIndexableResource sir) {
-        Object[] ref = new Object[7];
+        Object[] ref = new Object[INDEXABLES_XML_RES_COLUMNS.length];
         ref[COLUMN_INDEX_XML_RES_RANK] = sir.rank;
         ref[COLUMN_INDEX_XML_RES_RESID] = sir.xmlResId;
         ref[COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
@@ -68,6 +74,19 @@ public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvide
         ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "org.lineageos.settings.device";
         ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
         return ref;
+    }
+
+    private List<String> getNonIndexableKeys(Context context) {
+        List<String> keys;
+        if (!PackageManagerUtils.isAppInstalled(context, "org.lineageos.pocketmode")) {
+            keys.add(Constants.FP_POCKETMODE_KEY);
+        }
+        if (!FileUtils.fileExists(Constants.FP_HOME_KEY_NODE) &&
+                !FileUtils.fileExists(Constants.FP_WAKEUP_NODE)) {
+            keys.add(Constants.FP_HOME_KEY);
+            keys.add(Constants.FP_WAKEUP_NODE);
+        }
+        return keys;
     }
 
     @Override
@@ -79,6 +98,14 @@ public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvide
     @Override
     public Cursor queryNonIndexableKeys(String[] projection) {
         MatrixCursor cursor = new MatrixCursor(NON_INDEXABLES_KEYS_COLUMNS);
+        final Context context = getContext();
+
+        List<String> nonIndexableKeys = getNonIndexableKeys(context);
+        for (String nik : nonIndexableKeys) {
+            final Object[] ref = new Object[NON_INDEXABLES_KEYS_COLUMNS.length];
+            ref[COLUMN_INDEX_NON_INDEXABLE_KEYS_KEY_VALUE] = nik;
+            cursor.addRow(ref);
+        }
         return cursor;
     }
 }

Cosmic-Devices/android_device_xiaomi_

configpanel/src/com/cyanogenmod/settings/device/ConfigPanelSearchIndexablesProvider.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- *           (C) 2017 The LineageOS Project
+ *           (C) 2017-2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,11 +17,13 @@
 
 package com.cyanogenmod.settings.device;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.provider.SearchIndexableResource;
 import android.provider.SearchIndexablesProvider;
 
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_NON_INDEXABLE_KEYS_KEY_VALUE;
 import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_CLASS_NAME;
 import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_ICON_RESID;
 import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_ACTION;
@@ -32,6 +34,12 @@ import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RES
 import static android.provider.SearchIndexablesContract.INDEXABLES_RAW_COLUMNS;
 import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS;
 import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
+
+import com.cyanogenmod.settings.device.utils.FileUtils;
+import com.cyanogenmod.settings.device.utils.PackageManagerUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvider {
     private static final String TAG = "ConfigPanelSearchIndexablesProvider";
@@ -59,7 +67,7 @@ public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvide
     }
 
     private static Object[] generateResourceRef(SearchIndexableResource sir) {
-        Object[] ref = new Object[7];
+        final Object[] ref = new Object[INDEXABLES_XML_RES_COLUMNS.length];
         ref[COLUMN_INDEX_XML_RES_RANK] = sir.rank;
         ref[COLUMN_INDEX_XML_RES_RESID] = sir.xmlResId;
         ref[COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
@@ -68,6 +76,19 @@ public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvide
         ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "com.cyanogenmod.settings.device";
         ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
         return ref;
+    }
+
+    private List<String> getNonIndexableKeys(Context context) {
+        List<String> keys = new ArrayList<>();
+        if (!PackageManagerUtils.isAppInstalled(context, "com.cyanogenmod.pocketmode")) {
+            keys.add(Constants.FP_POCKETMODE_KEY);
+        }
+        if (!FileUtils.fileExists(Constants.FP_HOME_KEY_NODE) &&
+                !FileUtils.fileExists(Constants.FP_WAKEUP_NODE)) {
+            keys.add(Constants.FP_HOME_KEY);
+            keys.add(Constants.FP_WAKEUP_KEY);
+        }
+        return keys;
     }
 
     @Override
@@ -79,6 +100,14 @@ public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvide
     @Override
     public Cursor queryNonIndexableKeys(String[] projection) {
         MatrixCursor cursor = new MatrixCursor(NON_INDEXABLES_KEYS_COLUMNS);
+        final Context context = getContext();
+
+        List<String> nonIndexableKeys = getNonIndexableKeys(context);
+        for (String nik : nonIndexableKeys) {
+            final Object[] ref = new Object[NON_INDEXABLES_KEYS_COLUMNS.length];
+            ref[COLUMN_INDEX_NON_INDEXABLE_KEYS_KEY_VALUE] = nik;
+            cursor.addRow(ref);
+        }
         return cursor;
     }
 }